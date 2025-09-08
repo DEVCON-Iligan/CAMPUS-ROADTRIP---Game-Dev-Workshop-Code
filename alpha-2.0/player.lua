4eb02af90eee7e98b7e94fb53ec6684303c6e2db
Drei Abmab <dreiabmab@gmail.com>
2025-09-08 13:59:26 +0800

    Player Colission is active

--- alpha-2.0/player.lua
+++ alpha-2.0/player.lua
@@ -8,7 +8,7 @@ function Player:load()
     self.height = 18
 
     self.xVel = 0
-    self.yVel = 0
+    self.yVel = 100
 
     self.maxSpeed = 200
     self.acceleration = 4000
@@ -19,23 +19,20 @@ function Player:load()
     self.physics.body:setFixedRotation(true)
     self.physics.shape = love.physics.newRectangleShape(self.width, self.height)
     self.physics.fixture = love.physics.newFixture(self.physics.body, self.physics.shape)
-
 end
 
 function Player:update(dt)
-    self.syncPhysics()
-
+    self:syncPhysics()
 end
-
 
 function Player:syncPhysics()
     -- self.x = self.physics.body:getX()
     -- self.y = self.physics.body:getY()
 
-    self.x, self.y = self.physics.Body:getPosition()
-    
-
+    self.x, self.y = self.physics.body:getPosition()
+    self.physics.body:setLinearVelocity(self.xVel, self.yVel)
+end
 
 function Player:draw()
-    love.graphics.rectangle("fill", self.x, self.y, self.width, self.height)
+    love.graphics.rectangle("fill", self.x - self.width / 2, (self.y + (0.05 * self.height))  - self.height / 2, self.width, self.height)
 end
