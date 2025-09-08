1e3547e424b4c823c8ccb10143ef92ea847973da
Drei Abmab <dreiabmab@gmail.com>
2025-09-08 13:26:06 +0800

    Create alpha 2.0

---
+++ alpha-2.0/main.lua
@@ -0,0 +1,44 @@
+local STI = require("sti")
+
+function love.load()
+    -- 1
+    Map = STI("map/1.lua", {"box2d"})
+
+    -- 3
+    World = love.physics.newWorld( 0, 0 )
+    Map:box2d_init(World)
+    Map.layers.solid.visible = false
+
+    -- 5
+    background = love.graphics.newImage("assets/background/backdrop.png")
+    background:setFilter("nearest", "nearest")
+end
+
+function love.update(dt)
+    -- 4
+    World:update(dt)
+
+end
+
+function love.draw()
+    -- 6
+    local winW, winH = love.graphics.getWidth(), love.graphics.getHeight()
+    local imgW, imgH = background:getWidth(), background:getHeight()
+
+    local sx = winW / imgW  -- scale so image width matches window
+    local sy = sx   -- keep aspect ratio
+
+    local x = 0
+    local y = (winH - imgH * sy) / 2  -- center vertically (can be 0 to align top)
+
+    love.graphics.draw(background, x, y, 0, sx, sy)
+
+    -- 2 (open tile editor)
+    Map:draw(0, 0, 2, 2)
+
+    -- Support scalling
+    love.graphics.push()
+    love.graphics.scale(2, 2)
+    love.graphics.pop()
+
+end
---
+++ alpha-2.0/player.lua
@@ -0,0 +1,14 @@
+Player = {}
+
+function Player:load:()
+
+end
+
+function Player:update(dt)
+
+
+end
+
+function Player:draw()
+
+end
